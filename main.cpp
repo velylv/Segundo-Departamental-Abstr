e635c7154dd46a4cc35c12fdb4c472e6b45079f1
velylv <eved.2321@gmail.com>
2026-04-20 23:14:06 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,12 +1,54 @@
 #include "PROMEDIO.h"
+#include <stdio.h>
 
 int main(){
 
     Promedio p;
+    int opcion;
+    int leido = 0, calculado = 0;
 
-    p.leer();
-    p.calcular();
-    p.mostrar();
+    do{
+        printf("\n.: Menu de Promedio (Nuevo Tipo de Dato) :.\n");
+        printf("1.- Leer datos\n");
+        printf("2.- Calcular\n");
+        printf("3.- Mostrar resultados\n");
+        printf("4.- Salir\n");
+        printf("Opcion: ");
+        scanf("%d", &opcion);
+
+        switch(opcion){
+            case 1:
+                p.leer();
+                leido = 1;
+                calculado = 0;
+                break;
+
+            case 2:
+                if(leido){
+                    p.calcular();
+                    calculado = 1;
+                } else {
+                    printf("Primero debes leer los datos\n");
+                }
+                break;
+
+            case 3:
+                if(calculado){
+                    p.mostrar();
+                } else {
+                    printf("Primero debes calcular\n");
+                }
+                break;
+
+            case 4:
+                printf("Saliendo...\n");
+                break;
+
+            default:
+                printf("Opcion no valida\n");
+        }
+
+    } while(opcion != 4);
 
     return 0;
 }
